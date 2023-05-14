abba8225d0c8f9af8caea3de3f64ff7736b1d330
walid hamdi <walidhamdidev@gmail.com>
2023-05-14 13:21:19 +0100

    Handle when click submit to hide the keyboard

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import { useRef } from "react";
+import { KeyboardEvent, useRef, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
 interface Props {
@@ -8,13 +8,36 @@ interface Props {
 
 const SearchInput = ({ onSearchTerm }: Props) => {
   const searchInputRef = useRef<HTMLInputElement>(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleBlur = () => {
+    if (searchInputRef.current) {
+      searchInputRef?.current?.blur();
+    }
+  };
+
+  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && searchInputRef.current) {
+      searchInputRef?.current?.blur();
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSearchTerm(searchTerm);
+    setSearchTerm("");
+    // Clear the input field after the user submits the search
+    if (searchInputRef.current) {
+      searchInputRef.current.value = "";
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        onSearchTerm(searchInputRef?.current?.value ?? "");
-      }}
-    >
+    <form onSubmit={handleSubmit}>
       <InputGroup>
         <InputLeftElement children={<BsSearch />} />
         <Input
@@ -22,6 +45,10 @@ const SearchInput = ({ onSearchTerm }: Props) => {
           borderRadius={20}
           variant="filled"
           placeholder="Search games..."
+          onBlur={handleBlur}
+          onKeyPress={handleKeyPress}
+          value={searchTerm}
+          onChange={handleChange}
         />
       </InputGroup>
     </form>
